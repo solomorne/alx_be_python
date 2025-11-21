c36535aef41b49a116ba4e1efca598b6eebc64f4
solomon amaechi <solomornejohn@gmail.com>
2025-11-21 14:23:25 +0100

    shopping list manager

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,56 @@
+# Create an empty list
+shopping_list =[]
+
+# Define a function to add items to shopping list
+def add_item(item):
+    shopping_list.append(item)
+    print(f"{item} added to list")
+
+# Define a function to remove items from shopping list
+def remove_item(item):
+    if item in shopping_list:
+        shopping_list.remove(item)
+        print(f"{item} removed from list")
+    else:
+        print(f"{item} not found in shopping list")
+
+# Define a function to display items in shopping list
+def display_list():
+    if shopping_list:
+        for item in shopping_list:
+            print(f"{item}")
+    else:
+        print("Your shopping list is empty")
+
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Select a number (1-4): ")
+
+        if choice == '1':
+            # Prompt for and add an item
+            item = input("Enter an item to add: ")
+            add_item(item)
+        elif choice == '2':
+            # Prompt for and remove an item
+            item = input("Enter an item to remove: ")
+            remove_item(item)
+        elif choice == '3':
+            # Display the shopping list
+            display_list
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
