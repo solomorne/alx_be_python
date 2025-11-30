5b71eff66f7c6322440892b15bb78f6c46c28d3c
solomon amaechi <solomornejohn@gmail.com>
2025-11-30 05:28:01 +0100

    test simple calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -8,59 +8,40 @@ class TestSimpleCalculator(unittest.TestCase):
         self.calc = SimpleCalculator()
 
     # --- Addition Tests ---
-    def test_addition_positive_numbers(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
-
-    def test_addition_negative_numbers(self):
         self.assertEqual(self.calc.add(-2, -3), -5)
-
-    def test_addition_mixed_signs(self):
         self.assertEqual(self.calc.add(-1, 1), 0)
-
-    def test_addition_with_zero(self):
         self.assertEqual(self.calc.add(0, 5), 5)
 
+
     # --- Subtraction Tests ---
-    def test_subtraction_positive_numbers(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 3), 7)
-
-    def test_subtraction_negative_numbers(self):
         self.assertEqual(self.calc.subtract(-5, -2), -3)
-
-    def test_subtraction_mixed_signs(self):
         self.assertEqual(self.calc.subtract(5, -3), 8)
-
-    def test_subtraction_with_zero(self):
         self.assertEqual(self.calc.subtract(0, 7), -7)
 
+
+
     # --- Multiplication Tests ---
-    def test_multiplication_positive_numbers(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(4, 5), 20)
-
-    def test_multiplication_negative_numbers(self):
         self.assertEqual(self.calc.multiply(-4, -5), 20)
-
-    def test_multiplication_mixed_signs(self):
         self.assertEqual(self.calc.multiply(-4, 5), -20)
-
-    def test_multiplication_with_zero(self):
         self.assertEqual(self.calc.multiply(0, 100), 0)
 
+        
+
     # --- Division Tests ---
-    def test_division_positive_numbers(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
-
-    def test_division_negative_numbers(self):
         self.assertEqual(self.calc.divide(-10, -2), 5)
-
-    def test_division_mixed_signs(self):
         self.assertEqual(self.calc.divide(-10, 2), -5)
-
-    def test_division_with_zero_numerator(self):
         self.assertEqual(self.calc.divide(0, 5), 0)
-
-    def test_division_by_zero(self):
         self.assertIsNone(self.calc.divide(10, 0))  # Should return None
+
+        
 
 if __name__ == "__main__":
     unittest.main()
